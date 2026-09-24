4ec85a7ed290e65eb79ebec377270aec4c48d394
Matsunaga0910 <k.matsunaga.its.202604@gmail.com>
2026-09-24 09:29:26 +0900

    add:Case04.java　ログイン画面への遷移、受講生ユーザーでログインしコース詳細画面に遷移、上部メニューの「ヘルプ」リンクからヘルプ画面への遷移、「よくある質問」リンクからよくある質問画面を別タブで開く、4つのテストコードを追加

--- test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
+++ test_code_creation_name/src/test/java/jp/co/sss/lms/ct/f02_faq/Case04.java
@@ -1,6 +1,7 @@
 package jp.co.sss.lms.ct.f02_faq;
 
 import static jp.co.sss.lms.ct.util.WebDriverUtils.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.openqa.selenium.By;
 
 /**
  * 結合テスト よくある質問機能
@@ -35,28 +37,41 @@ public class Case04 {
 	@Order(1)
 	@DisplayName("テスト01 トップページURLでアクセス")
 	void test01() {
-		// TODO ここに追加
+		goTo("http://localhost:8888/lms");
+		assertEquals("ログイン | LMS", webDriver.getTitle());
+		getEvidence(new Object() {});
 	}
 
 	@Test
 	@Order(2)
 	@DisplayName("テスト02 初回ログイン済みの受講生ユーザーでログイン")
 	void test02() {
-		// TODO ここに追加
+		webDriver.findElement(By.id("loginId")).sendKeys("StudentAA02");
+		webDriver.findElement(By.id("password")).sendKeys("studentAA02");
+		webDriver.findElement(By.className("btn-primary")).click();
+		
+		pageLoadTimeout(10);
+		
+		assertEquals("コース詳細 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {});
 	}
 
 	@Test
 	@Order(3)
 	@DisplayName("テスト03 上部メニューの「ヘルプ」リンクからヘルプ画面に遷移")
 	void test03() {
-		// TODO ここに追加
+		goTo("http://localhost:8888/lms/help");
+		assertEquals("ヘルプ | LMS", webDriver.getTitle());
+		getEvidence(new Object() {});
 	}
 
 	@Test
 	@Order(4)
 	@DisplayName("テスト04 「よくある質問」リンクからよくある質問画面を別タブに開く")
 	void test04() {
-		// TODO ここに追加
+		goTo("http://localhost:8888/lms/faq");
+		assertEquals("よくある質問 | LMS", webDriver.getTitle());
+		getEvidence(new Object() {});
 	}
 
 }
